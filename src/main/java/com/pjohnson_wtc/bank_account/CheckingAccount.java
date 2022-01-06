0c7d582bb5b0231087eef31a1bc00f01a5a1a769
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2022-01-06 09:19:08 +0000

    Add comments

--- src/main/java/com/pjohnson_wtc/bank_account/CheckingAccount.java
+++ src/main/java/com/pjohnson_wtc/bank_account/CheckingAccount.java
@@ -21,11 +21,13 @@ public class CheckingAccount extends Account implements Interest {
 		return baseInterestRate.multiply(new BigDecimal(0.15));
 	}
 	
+	//WTCET-36 - NEW until 28
 	//toString - not inherited from Account as info specific to account type
 		public String toString() {
 			return "Name: " + name + ", Checking Account Number: " + accountNumber + ", Debit Card Number: " + debitCard.getIdNumber() + ", Balance: " + balance;
 		}
 	
+	//Necessary getters
 	public BankingTool getDebitCard() {
 		return debitCard;
 	}
